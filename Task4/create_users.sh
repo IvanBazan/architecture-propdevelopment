a8edc71d8c363c76754e4e319201775b994b33f5
ibazanov <ibazanov@cloudfactory.ru>
2025-07-26 17:06:07 +0300

    Task4

---
+++ Task4/create_users.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAMESPACE="rbac-propdev"
+
+# Создание service accounts
+kubectl create serviceaccount admin-user -n $NAMESPACE
+kubectl create serviceaccount reader-user -n $NAMESPACE
+kubectl create serviceaccount writer-user -n $NAMESPACE
+
+echo "ServiceAccounts created in namespace $NAMESPACE:"
+kubectl get serviceaccount -n $NAMESPACE | grep -E 'admin-user|reader-user|writer-user'
